ca05c8b090538ed935a0a6ae8e292dac888b3d47
kenpos <kenpos13@gmail.com>
2021-05-08 03:32:48 +0900

    Hwlo

--- jumanppGo.go
+++ jumanppGo.go
@@ -1,34 +1,20 @@
-package juman
+package jumanappGo
 
-import (
-	"fmt"
-	"os/exec"
-)
+// func Jumanpp(tweet string) string {
+// 	tweetstr := tweet
+// 	cmdstr := "echo " + tweetstr + "|jumanpp.exe --model=jumandic.jppmdl --force-single-path"
+// 	stdout, err := exec.Command("sh", "-c", cmdstr).Output()
 
-func jumanpp(tweet Tweet) int64 {
-	tweetstr := tweet.full_text
-	cmdstr := "echo " + tweetstr + "|jumanpp.exe --model=jumandic.jppmdl --force-single-path"
-	stdout, err := exec.Command("sh", "-c", cmdstr).Output()
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	// fmt.Printf("ls result: \n%s", string(stdout))
 
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(tweetstr)
-	anal(string(stdout))
-	// fmt.Printf("ls result: \n%s", string(stdout))
+// 	return string(stdout)
+// }
 
-	return 0
-}
+import "fmt"
 
-func Jumanpp(tweet Tweet) string {
-	tweetstr := tweet
-	cmdstr := "echo " + tweetstr + "|jumanpp.exe --model=jumandic.jppmdl --force-single-path"
-	stdout, err := exec.Command("sh", "-c", cmdstr).Output()
-
-	if err != nil {
-		fmt.Println(err)
-	}
-	// fmt.Printf("ls result: \n%s", string(stdout))
-
-	return string(stdout)
+func Hello(name string) {
+	fmt.Printf("Hello, %s!\n", name)
 }
