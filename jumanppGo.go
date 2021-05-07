57f53ed572480b764f2b1b9f800e9f49f57ad983
kenpos <kenpos13@gmail.com>
2021-05-08 03:38:24 +0900

    app

--- jumanppGo.go
+++ jumanppGo.go
@@ -1,17 +1,17 @@
 package jumanappGo
 
-// func Jumanpp(tweet string) string {
-// 	tweetstr := tweet
-// 	cmdstr := "echo " + tweetstr + "|jumanpp.exe --model=jumandic.jppmdl --force-single-path"
-// 	stdout, err := exec.Command("sh", "-c", cmdstr).Output()
+func Jumanpp(tweet string) string {
+	tweetstr := tweet
+	cmdstr := "echo " + tweetstr + "|jumanpp.exe --model=jumandic.jppmdl --force-single-path"
+	stdout, err := exec.Command("sh", "-c", cmdstr).Output()
 
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// 	// fmt.Printf("ls result: \n%s", string(stdout))
+	if err != nil {
+		fmt.Println(err)
+	}
+	// fmt.Printf("ls result: \n%s", string(stdout))
 
-// 	return string(stdout)
-// }
+	return string(stdout)
+}
 
 import "fmt"
 
