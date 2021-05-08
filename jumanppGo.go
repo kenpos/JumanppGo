0b4e43175286b4e9ccc48b92c2e46151a36fdf66
kenpos <kenpos13@gmail.com>
2021-05-08 15:26:31 +0900

    これでいいかな

--- jumanppGo.go
+++ jumanppGo.go
@@ -7,28 +7,28 @@ import (
 )
 
 type StandardDic struct {
-	midasi   string
-	yomi     string
-	genkei   string
-	hinsi    string
-	bunrui   string
-	katuyou1 string
-	katuyou2 string
-	imis     string
-	repname  string
+	Midasi   string
+	Yomi     string
+	Genkei   string
+	Hinsi    string
+	Bunrui   string
+	Katuyou1 string
+	Katuyou2 string
+	Imis     string
+	Repname  string
 }
 
 func set(n []string) StandardDic {
 	var s StandardDic
-	s.midasi = n[0]
-	s.yomi = n[1]
-	s.genkei = n[2]
-	s.hinsi = n[3]
-	s.bunrui = n[4]
-	s.katuyou1 = n[5]
-	s.katuyou2 = n[6]
-	s.imis = n[7]
-	s.repname = n[8]
+	s.Midasi = n[0]
+	s.Yomi = n[1]
+	s.Genkei = n[2]
+	s.Hinsi = n[3]
+	s.Bunrui = n[4]
+	s.Katuyou1 = n[5]
+	s.Katuyou2 = n[6]
+	s.Imis = n[7]
+	s.Repname = n[8]
 	return s
 }
 
